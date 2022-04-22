cb431fe7305a4cafc078f3521d8b4b0f448146c1
Vsirotkin <vsirotkin15@gmail.com>
2022-04-22 12:05:27 +0300

    os.environ in settings

--- ch4-message-board-app/django_project/settings.py
+++ ch4-message-board-app/django_project/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-'''
+
 
 DATABASES = {
     'default': {
@@ -94,7 +94,18 @@ DATABASES = {
         'PORT': 5432 # default postgres port
     }
 }
+'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
